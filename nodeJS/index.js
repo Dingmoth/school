8b248c6b7b5b085a21740c1d43eaa0dd0f48154f
Dingmoth <35528414+Dingmoth@users.noreply.github.com>
2023-03-03 13:02:01 +0200

    Add files via upload

--- nodeJS/index.js
+++ nodeJS/index.js
@@ -1,5 +1,26 @@
-console.log();
 
 const logger = require('./logger');
-
 logger.log('viskas veikia');
+
+//////
+
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+emitter.on('messageLogged', function(){
+    console.log('Gauta zinute');
+});
+emitter.emit('messageLogged');
+
+//register a listener. Need to be on the first place than .emit()
+emitter.on('messageLogged', (eventArg) => {
+    console.log("Pakviestas", eventArg);
+});
+
+//raise an event, can have an argument of
+emitter.emit('messageLogged', {id:1, url: "nera url :("});
+
+
+
+
+
